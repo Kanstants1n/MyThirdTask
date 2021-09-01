7961fb5ac1b7fe06cc35634a80c473f78083ad5f
Hilevich-Konstantin <k.hilevich@qa-academy.by>
2021-09-01 16:32:09 +0300

    commit

---
+++ MyThirdTask/src/test/java/Utils/AnyJSScripts.java
@@ -0,0 +1,7 @@
+package Utils;
+
+public class AnyJSScripts {
+    public static final String jsClick = "arguments[0].click()";
+    public static final String jsScroll = "arguments[0].scrollIntoView(true)";
+
+}
